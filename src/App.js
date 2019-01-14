8e0137dbf1459d94ce62f96f3b427ad3b5eded85
matthew-leee <psynergy.lth@gmail.com>
2019-01-14 21:16:32 +0800

    pull xd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import IgFetch from "./IgFetch"
+import Home from "./home"
+import AddPhotos from "./addPhotos"
+import * as ReactDOM from 'react-dom'
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  match
+} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Route exact path={"/"} component = {Home} />
+        <Route path = {"/addPhotos"} component = {AddPhotos} />
+        <Route path = {"/igFetch"} component = {IgFetch} />
+      </BrowserRouter>
     );
   }
 }
