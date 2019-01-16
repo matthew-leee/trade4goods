f2ba0a61f05e15e1d6f85dbcd04f59bceacc44b7
matthew-leee <psynergy.lth@gmail.com>
2019-01-16 11:32:54 +0800

    some failure with uploadphoto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import IgFetch from "./IgFetch"
-import Home from "./home"
-import AddPhotos from "./addPhotos"
-import * as ReactDOM from 'react-dom'
+import Home from "./Home"
+import AddPhotos from "./AddPhotos/AddPhotos"
+import { createStore, Action, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
+import photoReducers from "./AddPhotos/reducers"
+import actions from "./AddPhotos/actions"
+
 import {
   BrowserRouter,
   Route,
   Link,
-  match
+  Switch
 } from 'react-router-dom'
 import './App.css';
+
+const initState = {
+  details: [],
+  uploadedPhotos: [],
+  previewPhotos: []
+}
+
+const reducer = photoReducers(initState, actions)
+
+const store = createStore(photoReducers(initState, actions), applyMiddleware(logger))
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <Route exact path={"/"} component = {Home} />
-        <Route path = {"/addPhotos"} component = {AddPhotos} />
-        <Route path = {"/igFetch"} component = {IgFetch} />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+
+            <div className="links">
+              <p><Link to="/">Home</Link></p>
+              <p><Link to="/addPhotos">Add Photos</Link></p>
+              <p><Link to="/igFetch">Fetch ig AC(Beta function)</Link></p>
+            </div>
+
+            <Route exact path={"/"} component={Home} />
+            <Route path={"/addPhotos"} component={AddPhotos} />
+            <Route path={"/igFetch"} component={IgFetch} />
+
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
