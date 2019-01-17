4f4c7f45e1dda629c4896a874b226504c654bd69
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2019-01-17 23:09:49 +0800

    basic login pop up comp

---
+++ src/js/component/compCSS/popupCss.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export const popUpCloseTag = {
+    cursor: 'pointer',
+    position: 'absolute',
+    display: 'block',
+    padding: '2px 5px',
+    lineHeight: '20px',
+    right: '-10px',
+    top: '-10px',
+    fontSize: '20px',
+    background: '#ffffff',
+    borderRadius: '18px',
+    border: '1px solid #cfcece'
+}
+
+
+export const content = {
+    width: '100%',
+    padding: '12px'
+}
