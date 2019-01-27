2d82774848c9baa2f593d62c505164912d1f92f6
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2019-01-28 00:31:59 +0800

    add animation when loading image

--- src/js/component/MainCard.js
+++ src/js/component/MainCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Icon,Avatar  } from 'antd';
+import { Card, Icon,Avatar,Skeleton  } from 'antd';
 const { Meta } = Card;
 
 class MainCard extends React.Component {
@@ -7,14 +7,36 @@ class MainCard extends React.Component {
         super(props)
         this.state = {
             loading: true,
+            error:false,
+            imgPic: require('./asset/gif/loadingpic.gif')
           }
         
     }
+    componentDidMount() {
+        const img = new Image();
+        img.src = this.props.imgUrl;
+        img.onload = () => {
+          this.setState({
+            loading: false,
+            imgPic: img.src
+          });
+        };
+        img.onerror = () => {
+          this.setState({
+            error: true
+          });
+        };
+      }
     render() {
+
+   
+
         return (
-            <Card hoverable style={{ width: "100%", marginTop:"10px"}} cover={<img alt="example" src={this.props.imgUrl} />} 
+            <Card hoverable style={{ width: "100%", marginTop:"10px"}} cover={<img alt="example" src={this.state.imgPic} />} 
             actions={[<Icon type="heart" />, <Icon type="message" />, <Icon type="share-alt" />]} >
+            <Skeleton loading={this.state.loading}   title paragraph active>
                 <Meta title={this.props.name} description={this.props.description} avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}/>
+            </Skeleton>
             </Card>
         )
 
