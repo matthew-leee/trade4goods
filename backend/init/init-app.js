42f068475400e40b2ff8b94ed774ea9f3af24989
Jackykit123 <jackykit19970402@gmail.com>
2019-01-28 14:39:55 +0800

    edited deployment path

--- backend/init/init-app.js
+++ backend/init/init-app.js
@@ -1,6 +1,7 @@
 'use strict';
+const path = require('path')
 module.exports = (app, express, bodyParser, cors, router) => {
-    app.use(express.static('public'));
+    app.use(express.static(path.join(__dirname,'..','public')));
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(cors());
--- backend/routers/router.js
+++ backend/routers/router.js
@@ -4,5 +4,6 @@ module.exports = (express, authService, routerService) => {
     const router = express.Router();
     require('./authRouter')(router, authService)
     require('./profileRouter')(router, authService, routerService)
+    router.get('/*', (req, res) => res.sendFile(path.join(__dirname, '..','public','index.html')));
     return router;
 };
