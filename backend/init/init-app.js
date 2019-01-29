67f99ad5c6d5b873cc3d33068233ecc26a9a85c0
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2019-01-30 00:42:34 +0800

    get some gif

--- backend/init/init-app.js
+++ backend/init/init-app.js
@@ -3,9 +3,8 @@ module.exports = (app, express, bodyParser, cors, router, path) => {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(cors({
-        // origin: `http://${process.env.FRONTEND_URL}`,
         origin: String(process.env.FRONTEND_URL),
-        credentials: true 
+        credentials: false 
     }));
     app.use(router);
 }
