81b7046de3e0f960327ebb0992e70dbf552b5c6d
Jackykit123 <jackykit19970402@gmail.com>
2019-01-30 18:04:32 +0800

    added mailing notifications

---
+++ backend/services/product/mailNotify.js
@@ -0,0 +1,55 @@
+module.exports = class {
+    constructor(nodemailer, knex) {
+        this.nodemailer = nodemailer;
+        this.knex = knex
+        this.transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: process.env.MAIL_PORT,
+            secure: process.env.SECURITY,
+            auth: {
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD,
+            }
+        })
+    }
+
+    async sendOfferNotification(product_id) {
+        try {
+            let product = await this.knex('product').where('product_id', product_id)
+            product = product[0]
+            let user = await this.knex('users_credential').where('user_id', product.uploaded_by)
+            user = user[0]
+            const mailOptions = {
+                from: '"Testing" <Testing>',
+                to: user.email,
+                subject: 'Your Product has recieved offer', // Subject line
+                html: `<p>Heads Up!Your product ${product_name} has been offered<p>` +
+                    `<p>Visit <a>https://${process.env.FRONTEND_URL}/userPage</a>to view your offer</p>` +
+                    `<p>This is an automatic message sent by system, please do not reply to this message</p>`// html body
+            };
+            this.transporter.sendMail(mailOptions)
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async sendAcceptOfferNotification(product_id) {
+        try {
+            let product = await this.knex('product').where('product_id', product_id)
+            product = product[0]
+            let user = await this.knex('users_credential').where('user_id', product.uploaded_by)
+            user = user[0]
+            const mailOptions = {
+                from: '"Testing" <Testing>',
+                to: user.email,
+                subject: 'Your Offer has been Accepted', // Subject line
+                html: `<p>Heads Up!Your offer on roduct ${product_name} has been accept<p>` +
+                    `<p>Visit <a>https://${process.env.FRONTEND_URL}</a>to manage the trade</p>` +
+                    `<p>This is an automatic message sent by system, please do not reply to this message</p>`// html body
+            };
+            this.transporter.sendMail(mailOptions)
+        } catch (err) {
+            throw err
+        }
+    }
+}
