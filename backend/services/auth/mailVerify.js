d28c6e9022b5d89d063fbb14ba70d5868fdee311
Jackykit123 <jackykit19970402@gmail.com>
2019-01-30 13:57:28 +0800

    edited the mail address

--- backend/services/auth/mailVerify.js
+++ backend/services/auth/mailVerify.js
@@ -18,7 +18,7 @@ module.exports = class {
             to: email,
             subject: 'Verify Your Email Address', // Subject line
             html: '<p>Verify your email by clicking below<p>' + 
-            `<a href="https://${process.env.HOST}:${process.env.PORT}/verifyEmail/${key}">Click Me to Verify</a>` +
+            `<a href="https://${process.env.FRONTEND_URL}/verifyEmail/${key}">Click Me to Verify</a>` +
             `<p>This verification is valid for 24hours.</p>` +
             `<p>This is an automatic message sent by system, please do not reply to this message</p>`// html body
         };
@@ -32,7 +32,7 @@ module.exports = class {
             subject: 'Password Reset Request', // Subject line
             html: '<p>Reset Your password by clicking below<p>' +
             `<p><strong>Note, please ignore this email if you did not request to change the password</strong></p>` +
-            `<a href="https://${process.env.HOST}:${process.env.PORT}/resetPassword/${key}">Click Me to Reset</a>` + 
+            `<a href="https://${process.env.FRONTEND_URL}/resetPassword/${key}">Click Me to Reset</a>` + 
             `<p>This is an automatic message sent by system, please do not reply to this message</p>`// html body
         };
         this.transporter.sendMail(mailOptions)
