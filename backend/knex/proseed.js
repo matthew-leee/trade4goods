6582a8969da0efb4b0183ffd024ecd0eeca10ca5
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2019-02-01 09:45:36 +0800

    more realistic seed

---
+++ backend/knex/proseed.js
@@ -0,0 +1,75 @@
+const faker = require('faker');
+const path = require('path')
+require('dotenv').config({path: path.join(__dirname, '..', '.env')});
+const knex = require('knex')({
+    client: 'postgresql',
+    connection: {
+    database: process.env.DB_NAME,
+    user:     process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+  },
+});
+
+
+(async function excalibur() {
+    let productArr = [];
+    let seedingTable = 'products'
+    let fakeDataGenerateNumber = 1000
+
+
+    await knex(seedingTable).del();
+
+    for (let i = 1; i < fakeDataGenerateNumber; i++) {
+        let picUrl = 'https://dummyimage.com/' + Math.floor(Math.random() * (800 - 350) + 350) +'x'+Math.floor(Math.random() * (800 - 350) + 350)+ '/' + Math.floor(Math.random() * 999999) + '/' + Math.floor(Math.random() * 999999) + '.png'
+        let picUrl2 = 'https://dummyimage.com/' + Math.floor(Math.random() * (800 - 350) + 350) +'x'+Math.floor(Math.random() * (800 - 350) + 350)+  '/' + Math.floor(Math.random() * 999999) + '/' + Math.floor(Math.random() * 999999) + '.png'
+        let picUrl3 = 'https://dummyimage.com/' + Math.floor(Math.random() * (800- 350) + 350) +'x'+Math.floor(Math.random() * (800 - 350) + 350)+  '/' + Math.floor(Math.random() * 999999) + '/' + Math.floor(Math.random() * 999999) + '.png'
+        
+
+        
+        //product table
+        let name = faker.commerce.productName()
+        let image = randomPic()
+        let description = faker.lorem.paragraph().slice(0, 255)
+        let expectation = faker.lorem.sentences().slice(0, 255)
+        let trade_location = faker.lorem.word()
+        let tags = [faker.lorem.word(), faker.lorem.word(), faker.lorem.word(), faker.lorem.word()]
+        // let uploaded_by = i
+        let liked_by = []
+        let comments = []
+        let offered_by = []
+        let sold_to = null
+        let sold_at = null
+        
+        function randomPic(){
+            let key = name.split(" ").join(",")
+            let uu = 'https://source.unsplash.com/random/?' + key
+            let r = Math.floor(Math.random()* 3 )
+            if(r === 0){
+                return [uu]
+            }
+            if(r===1){
+                return[uu,uu]
+            }
+            if(r===2){
+                return[uu,uu,uu]
+            }
+            if(r===3){
+                return[uu,uu,uu]
+            }
+        }
+        
+        
+        let cool = { "name": name, "image": image, "description": description, "expectation": expectation, "trade_location": trade_location, "tags": tags, "liked_by": liked_by, "comments": comments, "offered_by": offered_by, "sold_to": sold_to, "sold_at": sold_at }
+        
+        productArr.push({ "name": name, "image": image, "description": description, "expectation": expectation, "trade_location": trade_location, "tags": tags, "liked_by": liked_by, "comments": comments, "offered_by": offered_by, "sold_to": sold_to, "sold_at": sold_at })
+        
+        console.log(cool)
+    }
+    const id = await knex.select("user_id").from("users")
+    const productArrWithId = productArr.map((u, i)=>{
+        u.uploaded_by = Math.floor(Math.random()* (id.length - 1) + 1)
+        return u
+    })
+    await knex(seedingTable).insert(productArrWithId)
+    console.log("success update table data: " + seedingTable)
+})();
