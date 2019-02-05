9dcac7145213eb058ec3d9d77760b916f20cc534
matthew-leee <psynergy.lth@gmail.com>
2019-02-05 23:29:56 +0800

    finish portfolio page

--- src/js/component/newUserPage/MyProf.js
+++ src/js/component/newUserPage/MyProf.js
@@ -4,13 +4,14 @@ import MyProfStyle from "./style/MyProf"
 import actions_newUserPage from "../../actions/newUserPage"
 
 // antd
-import { Icon } from "antd"
+import { Icon, Progress } from "antd"
 
 class MyProf extends Component {
 
     render() {
         return (
             <div className="MyProf" style={MyProfStyle.MyProf}>
+
                 <div className="profile" style={MyProfStyle.profile}>
 
                     <div className="img" style={MyProfStyle.imgClass}>
@@ -23,17 +24,45 @@ class MyProf extends Component {
                             </div>}
                         <img style={MyProfStyle.img} src="https://instagram.fhkg10-1.fna.fbcdn.net/vp/857f8f78aec79e847df2015101da19ed/5CF95050/t51.2885-15/e35/50875616_283040825708747_6865581596424783698_n.jpg?_nc_ht=instagram.fhkg10-1.fna.fbcdn.net" alt="" />
                     </div>
+
                     <div className="details" style={MyProfStyle.details}>
                         <p>name</p>
                         <h4 style={MyProfStyle.detailsh4}>{this.props.myUser.displayed_name}</h4>
                         <p>phone</p>
                         <h4 style={MyProfStyle.detailsh4}>{this.props.myUser.phone_number}</h4>
                     </div>
+
                 </div>
+
+
                 <div className="stat" style={MyProfStyle.stat}>
-                    some shig
+                    <div className="title" style={MyProfStyle.statTitle}>
+                        <h5 style={{ padding: 0, marginLeft: "0.5vw", marginBottom: 0 }}>Stats</h5>
                     </div>
-                <div className="history" style={MyProfStyle.history}>shit</div>
+                    <div className="stat1" style={MyProfStyle.stat1}>
+                        <div className="credProgress" style={MyProfStyle.cred}>
+                            <Progress type="circle" percent={this.props.myUser.credibility * 10} />
+                            <p style={{ marginBottom: 0, marginTop: "1vw" }}>Your Trade Credibility</p>
+                        </div>
+                    </div>
+                    <div className="stat2" style={MyProfStyle.stat2}>
+                        <div className="credProgress" style={MyProfStyle.cred}>
+                            <Progress type="circle" percent={this.props.myUser.credibility * 10} />
+                            <p style={{ marginBottom: 0, marginTop: "1vw" }}>Trader's Level</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="history" style={MyProfStyle.history}>
+                    <div className="title" style={MyProfStyle.historyTitle}>
+                        <h5 style={{ padding: 0, marginLeft: "0.5vw", marginBottom: 0 }}>Trade History</h5>
+                    </div>
+                    <div className="details" style={MyProfStyle.historyDetails}>
+                        {!this.props.myUser.trade_history &&
+                        <h5 style={{ padding: 0,  marginBottom: 0 }}>You have no Trade History. Start Bartering!</h5>}
+                    </div>
+                </div>
+
                 <div className="chain" style={MyProfStyle.chain}>asdf</div>
             </div>
         )
--- src/js/component/newUserPage/style/MyProf.js
+++ src/js/component/newUserPage/style/MyProf.js
@@ -1,5 +1,6 @@
 const MyProfStyle = {
     MyProf: {
+        fontFamily: "'Rajdhani', sans-serif",
         padding: "2.5vw 2.5vw 2.5vw 2.5vw",
         display: "grid",
         height: "inherits",
@@ -19,6 +20,7 @@ const MyProfStyle = {
         margin: "1vw 1vw 1vw 1vw",
         display: "flex",
         flexDirection: "column",
+        boxShadow: "0.5vw 0.5vw 4vw rgb(50,50,50)",
     },
     imgClass: {
         width: "19.25vw",
@@ -52,7 +54,7 @@ const MyProfStyle = {
         justifyContent: "center",
         alignItems: "center",
     },
-    img:{
+    img: {
         width: "19.25vw",
     },
     icon: {
@@ -63,25 +65,75 @@ const MyProfStyle = {
     details: {
         margin: "1vw 1vw 1vw 1vw"
     },
-    detailsh4:{
+    detailsh4: {
         paddingBottom: "1vw"
     },
     stat: {
         gridArea: "b",
-        backgroundColor: "yellow",
         margin: "1vw 1vw 1vw 1vw",
+        display: "grid",
+        gridTemplateColumns: "repeat(2, 1fr)",
+        gridTemplateRows: "repeat(6, 1fr)",
+        boxShadow: "0.5vw 0.5vw 4vw rgb(50,50,50)",
+        backgroundColor: "rgb(60,237,188)"
+    },
+    stat1: {
+        gridColumn: "1/2",
+        gridRow: "2/7",
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(150,150,150,0.4)"
+    },
+    cred: {
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+    },
+    stat2: {
+        gridColumn: "2/3",
+        gridRow: "2/7",
+        backgroundColor: "rgba(150,150,150,0.5)",
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    statTitle: {
+        gridColumn: "1/3",
+        gridRow: "1/2",
+        backgroundColor: "rgba(150,150,150,0.8)",
+        display: "flex",
+        alignItems: "center"
     },
     history: {
         gridArea: "c",
-        backgroundColor: "green",
+        backgroundColor: "rgb(181,229,80)",
         margin: "1vw 1vw 1vw 1vw",
+        boxShadow: "0.5vw 0.5vw 4vw rgb(50,50,50)",
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        gridTemplateRows: "repeat(6, 1fr)",
+    },
+    historyTitle:{
+        gridRow: "1/2",
+        backgroundColor: "rgba(150,150,150,0.8)",
+        display: "flex",
+        alignItems: "center"
+    },
+    historyDetails: {
+        gridRow: "2/7",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(150,150,150,0.6)",
     },
     chain: {
         gridArea: "d",
         backgroundColor: "blue",
         margin: "1vw 1vw 1vw 1vw",
+        boxShadow: "0.5vw 0.5vw 4vw rgb(50,50,50)",
     }
-    
 }
 
 export default MyProfStyle
