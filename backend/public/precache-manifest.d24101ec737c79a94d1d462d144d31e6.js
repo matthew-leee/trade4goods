888641163f28e7020aff9f425ce11c2d7605d1ca
Jackykit123 <jackykit19970402@gmail.com>
2019-02-11 14:38:48 +0800

    public folder added

---
+++ backend/public/precache-manifest.d24101ec737c79a94d1d462d144d31e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7af208322b59091365ce",
+    "url": "/static/css/main.cc3318c4.chunk.css"
+  },
+  {
+    "revision": "7af208322b59091365ce",
+    "url": "/static/js/main.7af20832.chunk.js"
+  },
+  {
+    "revision": "5f04ab48e30d5131fb82",
+    "url": "/static/css/1.b9c9a3b6.chunk.css"
+  },
+  {
+    "revision": "5f04ab48e30d5131fb82",
+    "url": "/static/js/1.5f04ab48.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c11c6e78289f646bc0e939ca42560e0",
+    "url": "/static/media/error404.9c11c6e7.gif"
+  },
+  {
+    "revision": "0c094ed47b5b59c532d010cdb0700226",
+    "url": "/static/media/replaceNoImg.0c094ed4.gif"
+  },
+  {
+    "revision": "2fe17f6aa1c6d93ee58d8d0b49d94753",
+    "url": "/static/media/loadingpic.2fe17f6a.gif"
+  },
+  {
+    "revision": "a7f3de545d15a804d728531e8ebf5c1f",
+    "url": "/static/media/mainPageLoadingPic.a7f3de54.gif"
+  },
+  {
+    "revision": "0d96e487ef3ebecb48f5ab1cd36feddf",
+    "url": "/index.html"
+  }
+];
