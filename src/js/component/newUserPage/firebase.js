4793acea749d1abbd0a0cc17b67111df04d1d097
matthew-leee <psynergy.lth@gmail.com>
2019-02-14 13:56:51 +0800

    pull

--- src/js/component/newUserPage/firebase.js
+++ src/js/component/newUserPage/firebase.js
@@ -1,7 +1,7 @@
 import firebaseConfig from "../../../firebase"
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+import * as firebase from "firebase"
 const app = firebase.initializeApp(firebaseConfig)
 
 export default app
